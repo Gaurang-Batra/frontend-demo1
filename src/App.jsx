46a61fa292f95fd30db6b627698085d07a463ff4
Gaurang Batra <155742509+Gaurang-Batra@users.noreply.github.com>
2025-05-30 10:09:23 +0530

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+// src/App.jsx
+function App() {
+  return (
+    <div style={{ textAlign: "center", marginTop: "50px" }}>
+      <h1>Welcome to My Azure Web App 🚀</h1>
+      <p>This is a demo React project deployed on Azure!</p>
+    </div>
+  );
+}
+
+export default App;
